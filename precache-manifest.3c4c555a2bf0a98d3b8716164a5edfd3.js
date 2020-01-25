39779d29375ce87f0bd3f37d21a3e6f33d4e78f6
Osvaldo villagomez <osvaldovillagomez@Osvaldos-MacBook-Pro.local>
2020-01-25 17:40:49 -0600

    Updates

---
+++ precache-manifest.3c4c555a2bf0a98d3b8716164a5edfd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658a83b36856daa556cf87b88bd3c530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bad06d931ae94fb3d52",
+    "url": "/static/css/main.18be5d0f.chunk.css"
+  },
+  {
+    "revision": "3d16574604b82b2a8c60",
+    "url": "/static/js/2.7531dae1.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.7531dae1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bad06d931ae94fb3d52",
+    "url": "/static/js/main.2de2affa.chunk.js"
+  },
+  {
+    "revision": "6a9b40654dc0f2cd4b83",
+    "url": "/static/js/runtime-main.ef817b37.js"
+  }
+]);
