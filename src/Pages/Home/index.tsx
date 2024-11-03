10a4d47d49e6d5fc1bca8df65ae042d113ec239a
Osvaldo villagomez <osvaldovillagomez@MacBook-Pro.local>
2024-11-03 14:50:16 -0600

    guapo easter egg

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import classes from './Home.module.css';
 import Page from '../../Components/Page';
 import MoonImage from '../../Assets/moon.jpeg';
+import LosGuaposImage from '../../Assets/los-guapos.jpeg';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const [image, setImage] = useState(MoonImage);
+  // const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      setInput((prev) => (prev + event.key).slice(-6)); // Keep only last 6 characters
+
+      if ((input + event.key).slice(-6) === 'guapo') {
+        setImage(LosGuaposImage);
+        // setTimeout(() => setImage(MoonImage), 6000); // Hide after 6s
+        setInput('');
+      }
+
+      // timeoutRef.current = setTimeout(() => setInput(''), 5000);
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+  }, [input]);
+
   return (
     <div>
       {/* TODO: delete when video is added */}
       <div
         className="fixed top-0 left-0 w-screen h-screen -z-50"
         style={{
-          background: `url(${MoonImage}) no-repeat center center/cover `,
+          background: `url(${image}) no-repeat center center/cover `,
         }}
       />
       <div className="relative h-dvh w-dvh overflow-hidden flex">
